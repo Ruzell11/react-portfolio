aaa64f3f2bc13ee1c72ab434bcc11b86688dd867
Ruzell11 <johnruzell123@gmail.com>
2022-08-04 18:04:12 +0800

    about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,28 @@
 import React from 'react';
 
 
-class About extends React.Component{
-    render(){
-        return(
-            <div name='About ' className='About d-flex justify-content-center align-items-center h-100 w-100 flex-column'>
-                <div className=''>
-                    <h1>About</h1>
-                </div>
-                <div>
-                    <p>I enjoy having a mastering a new things It boosts self-esteem.Learning is about commmitment on how passionate you are on what you are doing. Having a skills does not a power , actions with skills does. Experience on learning journey is absolutely I love the most!</p>
-                    <br/>
-                    <p>My goal is to have the opportunity to work for an organization that offers me the opportunity to improve my skills or learn new things. Ideally, the experiences</p>
+class About extends React.Component {
+    render() {
+        return (
+            <div name='about' id='about' className='about px-5 align-items-center justify-content-center'>
+                <div className="container d-flex justify-content-center align-items-center" style={{ height: '70vh' }}>
+                    <div className="row">
+                        <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-7 col-xxl-7 d-flex align-items-center justify-content-center">
+                            <div>
+                                <h1 className='fw-bold text-white mt-5 border-bottom w-50'>
+                                    About Me
+                                </h1>
+                                <p className='text-secondary'>I enjoy having a mastering a new things It boosts self-esteem.Learning is about commmitment on how passionate you are on what you are doing. Having a skills does not a power , actions with skills does. Experience on learning journey is absolutely I love the most!</p>
+                                <p className='text-secondary'>My goal is to have the opportunity to work for an organization that offers me the opportunity to improve my skills or learn new things. Ideally, the experiences</p>
+                            </div>
+                        </div>
+                        <div className='col-12 col-sm-12 col-md-12 col-lg-12 col-xl-5 col-xxl-5 d-flex justify-content-center align-items-center'>
+
+                        </div>
+                    </div>
                 </div>
             </div>
+
         )
     }
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Logo from '../assets/logo.png';
 import { FaBars, FaTimes } from 'react-icons/fa'
-import OutsideClickHandler from 'react-outside-click-handler';
-import Home from '../components/Home';
-import PropTypes from 'prop-types'
 import App from '../App';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './Home';
+import About from './About';
 
 
 class Navbar extends React.Component {
@@ -18,73 +17,113 @@ class Navbar extends React.Component {
     handleClick = (props) => {
         this.setState(prevState => ({
             navbar: !prevState.navbar,
-            
-        }));  
+
+        }));
     }
-    handleClose = () =>{
+    handleClose = () => {
         this.setState({
-            navbar:false
+            navbar: false
         })
-    }  
+    }
     links = [
         {
             id: 1,
-            link: 'Home'
+            link: 'Home',
+            to: 'home',
+            path: '/',
+            element: <Home />
         },
         {
             id: 5,
-            link: 'About'
+            link: 'About',
+            to: 'about',
+            path: '/about',
+            element: <About />
+
         },
         {
             id: 2,
-            link: 'Skills'
+            link: 'Skills',
+            to: 'skills',
+            path: '/skills',
+            element: <Home />
         },
         {
             id: 3,
-            link: 'Project'
+            link: 'Project',
+            to: 'project',
+            path: '/project',
+            element: <Home />
         },
 
         {
             id: 4,
-            link: 'Contact'
+            link: 'Contact',
+            to: 'contact',
+            path: '/contact',
+            element: <Home />
+
         }
     ]
     render(props) {
+
         return (
+            
             <div>
-            <div className='d-flex justify-content-between align-items-center p-4 nav' style={{ backgroundColor: 'black' }}>
-                <div className="logo mt-2">
-                    <h2 className='name'>JRUZEll</h2>
-                </div>
-                <div className=' d-none d-md-none d-sm-none d-lg-flex d-xxl-flex d-xl-flex mt-2 '>
-                    <ul className='  nav-link d-flex px-4 justify-content-center align-items-center '>
+              
+                <div className='d-flex justify-content-between align-items-center p-4 nav' style={{ backgroundColor: 'black' }}>
+                    <div className="logo mt-2">
+                        <h2 className='name'>JRUZEll</h2>
+                    </div>
+                    <div className=' d-none d-md-none d-sm-none d-lg-flex d-xxl-flex d-xl-flex mt-2 '>
+                    
+                        <ul className='  nav-link d-flex px-4 justify-content-center align-items-center '>
+                            {this.links.map(({ id, link, to , path , element }) => {
+                                return (
+                                    
+                                    <>
+                                    {/* <Router> */}
+                                        <li key={id} className='nav-link text-secondary fw-bolder fs-5 pe-auto' style={{ cursor: 'pointer' }}>{link}
+                                        
+                                        {/* <Link to={to} path={path} smooth duration={500}>{link}</Link> */}
+                                      
+                                    
+                                      
+                                         </li>
+                                         {/* </Router> */}
+                                  
+                                        
+                                        
+                                       
+                                    </>
+                                    
+                                )
+                            })}
+                        </ul>
+                        
+                    </div>
+                    {/* mobile */}
+                    <div onClick={this.handleClick} className='d-flex d-md-flex d-sm-flex d-lg-none d-xxl-none d-xl-none bars' >
+                        {!this.state.navbar ? <FaBars style={{ color: 'whitesmoke', fontSize: '2rem' }} /> : <FaTimes style={{ color: 'whitesmoke', fontSize: '2rem' }} />}
+                    </div>
+                    <ul className={!this.state.navbar ? 'd-none' :
+                        'position-absolute top-0 start-0  d-flex justify-content-center align-items-center flex-column d-md-flex d-sm-flex d-lg-none d-xxl-none d-xl-none w-100 navMobile'
+                    } style={{ marginTop: '5rem' }}>
                         {this.links.map(({ id, link }) => {
                             return (
                                 <>
-                                    <li key={id} className='nav-link text-secondary fw-bolder fs-5 pe-auto' style={{ cursor: 'pointer' }}>{link} </li>
+                                    <li key={id} className='fs-3 py-2 nav-link text-secondary' onClick={this.handleClick} style={{ cursor: 'pointer' }}>{link}</li>
                                 </>
                             )
                         })}
                     </ul>
                 </div>
-                {/* mobile */}
-                <div onClick={this.handleClick} className='d-flex d-md-flex d-sm-flex d-lg-none d-xxl-none d-xl-none bars' >
-                    {!this.state.navbar ? <FaBars style={{ color: 'whitesmoke', fontSize: '2rem' }} /> : <FaTimes style={{ color: 'whitesmoke', fontSize: '2rem' }} />}
-                </div>
-                        <ul className={!this.state.navbar ? 'd-none' :
-                            'position-absolute top-0 start-0  d-flex justify-content-center align-items-center flex-column d-md-flex d-sm-flex d-lg-none d-xxl-none d-xl-none w-100 navMobile'
-                        } style={{ marginTop: '5rem' }}>
-                            {this.links.map(({ id, link }) => {
-                                return (
-                                    <>
-                                        <li key={id} className='fs-3 py-2 nav-link text-secondary' onClick={this.handleClick} style={{cursor:'pointer'}}>{link}</li>
-                                    </>
-                                )
-                            })}
-                        </ul>
-                        </div>  
-                       <App  deleteHandler={this.handleClose} />
+               
+              
+                <App deleteHandler={this.handleClose} />
+
             </div>
+
         )
     }
 }
